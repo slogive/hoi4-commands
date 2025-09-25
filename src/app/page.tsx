8955de0eb51ea59a1959a98879bb5050d0887a80
Cesar Fonseca <cesar.fonseca@wearedev.co>
2025-09-24 20:17:45 -0500

    Add new assets and styles for Rocket Art items
    - Added Rocket_Art_1_allies.png and Rocket_Art_2_allies.png images to the public/items directory.
    - Created a new SCSS file for page styles, defining layout, padding, borders, and hover effects for item containers.
    - Implemented a grid layout for displaying items with responsive design considerations.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,194 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import { Button, Form, InputNumber, List, notification } from 'antd'
+import { useState } from 'react'
+
+import './page.scss'
+
+const items = [{}]
+
+const equipmentData = [
+	{
+		category: 'Infantry Equipment',
+		items: [
+			{
+				key: 'infantry_equipment_1',
+				label: 'Infantry Equipment 1',
+				img_src: '/items/Infantry0.png',
+			},
+			{
+				key: 'infantry_equipment_2',
+				label: 'Infantry Equipment 2',
+				img_src: '/items/Infantry1.png',
+			},
+			{
+				key: 'infantry_equipment_3',
+				label: 'Infantry Equipment 3',
+				img_src: '/items/Infantry2.png',
+			},
+			{
+				key: 'infantry_equipment_4',
+				label: 'Infantry Equipment 4',
+				img_src: '/items/Infantry3.png',
+			},
+		],
+	},
+	{
+		category: 'Artillery',
+		items: [
+			{
+				key: 'artillery_equipment_1',
+				label: 'Artillery Equipment 1',
+				img_src: '/items/Art_1_comintern.png',
+			},
+			{
+				key: 'artillery_equipment_2',
+				label: 'Artillery Equipment 2',
+				img_src: '/items/Art_2_comintern.png',
+			},
+			{
+				key: 'artillery_equipment_3',
+				label: 'Artillery Equipment 3',
+				img_src: '/items/Art_3_comintern.png',
+			},
+		],
+	},
+	{
+		category: 'Tank Destroyers / AT',
+		items: [
+			{
+				key: 'tank_destroyer_equipment_1',
+				label: 'Tank Destroyer Equipment 1',
+				img_src: '/items/AT_1_allies.png',
+			},
+			{
+				key: 'tank_destroyer_equipment_2',
+				label: 'Tank Destroyer Equipment 2',
+				img_src: '/items/AT_2_allies.png',
+			},
+			{
+				key: 'tank_destroyer_equipment_3',
+				label: 'Tank Destroyer Equipment 3',
+				img_src: '/items/AT_3_allies.png',
+			},
+		],
+	},
+	{
+		category: 'Anti-Air',
+		items: [
+			{
+				key: 'aa_gun_equipment_1',
+				label: 'AA Gun Equipment 1',
+				img_src: '/items/AA_1_allies.png',
+			},
+			{
+				key: 'aa_gun_equipment_2',
+				label: 'AA Gun Equipment 2',
+				img_src: '/items/AA_2_allies.png',
+			},
+			{
+				key: 'aa_gun_equipment_3',
+				label: 'AA Gun Equipment 3',
+				img_src: '/items/AA_3_allies.png',
+			},
+		],
+	},
+	{
+		category: 'Trucks / Motorized',
+		items: [
+			{
+				key: 'motorized_equipment_1',
+				label: 'Motorized Equipment 1 (Generic Trucks)',
+				img_src: '',
+			},
+			{
+				key: 'motorized_equipment_2',
+				label: 'Motorized Equipment 2 (Later-Tier Motorized)',
+				img_src: '',
+			},
+		],
+	},
+]
+
+const aeCommand = 'add_equipment'
+
+const openNotification = (message: string) => {
+	notification.success({
+		message: 'Command Copied',
+		description: message,
+		placement: 'topRight',
+	})
+}
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	const [count, setCount] = useState(1_000_000)
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	return (
+		<div className='page'>
+			<main className='main'>
+				<h1 className='title'>Hearts of Iron IV - Commands</h1>
+
+				<Form>
+					<Form.Item label='Count'>
+						<InputNumber
+							value={count}
+							onChange={(value) => setCount(value || 0)}
+						/>
+					</Form.Item>
+				</Form>
+
+				<List
+					className='items-list'
+					dataSource={equipmentData}
+					renderItem={(category) => (
+						<div
+							className='item-category'
+							key={category.category}
+						>
+							<span className='item-title'>{category.category}</span>
+
+							<div className='items-container'>
+								{category.items.map((item) => (
+									<div
+										key={item.key}
+										onClick={() => {
+											const command = `${aeCommand} ${item.key} ${count}`
+											navigator.clipboard.writeText(command)
+											openNotification(`Copied: ${command}`)
+										}}
+										className='item-container'
+									>
+										{item.img_src && (
+											<div
+												style={{
+													width: '103px',
+													height: '50px',
+													display: 'flex',
+													justifyContent: 'center',
+													alignItems: 'center',
+													backgroundColor: '#f0f0f0',
+												}}
+											>
+												<img
+													src={item.img_src}
+													alt={item.label}
+													style={{
+														maxWidth: '100%',
+														maxHeight: '100%',
+														objectFit: 'contain',
+													}}
+												/>
+											</div>
+										)}
+
+										<strong>{item.label}</strong>
+									</div>
+								))}
+							</div>
+						</div>
+					)}
+				/>
+			</main>
+		</div>
+	)
 }
